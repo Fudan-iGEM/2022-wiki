194bbfcd4b907f16cbbd2664e90d2815c4b9435a
Zihan Zhang <2364065915@qq.com>
2022-07-12 06:03:12 +0000

    尝试加入markdown转网页功能

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 from flask import Flask, render_template
 from flask_frozen import Freezer
+import markdown
 
 
 template_folder = path.abspath('./wiki')
@@ -29,6 +30,23 @@ def index():
 @app.route('/<page>')
 def pages(page):
     return render_template(str(Path('pages') / (page.lower() + '.html')))
+
+@app.route('/<newpage>')
+
+def markdown1(page):
+    template_folder = path.abspath('./wiki/markdown')
+    md = markdown.Markdown(
+    extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.toc'
+        ]
+    )
+    html = md.convert(text)
+    page = render_template(context={'body' : html, 'toc': md.toc})
+return render_template(str(Path('markdown') / (page.lower() + '.html')))
+
+def pages(page)
+    return render_template(markdown1(page) ,context={'body': markdown1(page)} )
 
 # Main Function, Runs at http://0.0.0.0:8080
 if __name__ == "__main__":
