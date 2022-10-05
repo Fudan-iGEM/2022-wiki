4b007642bff3b255c784735ae3de838bbd83391c
Liang Cai <coronin@gmail.com>
2022-10-05 20:26:13 +0800

    not using gitlab md, etc

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ import markdown
 template_folder = path.abspath('./wiki')
 
 app = Flask(__name__, template_folder=template_folder)
-#app.config['FREEZER_BASE_URL'] = environ.get('CI_PAGES_URL')
+####
+app.config['FREEZER_BASE_URL'] = environ.get('CI_PAGES_URL')
 app.config['FREEZER_DESTINATION'] = 'public'
 app.config['FREEZER_RELATIVE_URLS'] = True
 app.config['FREEZER_IGNORE_MIMETYPE_WARNINGS'] = True
@@ -31,22 +32,23 @@ def index():
 def pages(page):
     return render_template(str(Path('pages') / (page.lower() + '.html')))
 
-@app.route('/<newpage>')
-
-def markdown1(page):
-    template_folder = path.abspath('./wiki/markdown')
-    md = markdown.Markdown(
-    extensions=[
-            'markdown.extensions.extra',
-            'markdown.extensions.toc'
-        ]
-    )
-    html = md.convert(text)
-    page = render_template(context={'body' : html, 'toc': md.toc})
-    return render_template(str(Path('markdown') / (page.lower() + '.html')))
-
-def pages(page):
-    return render_template(markdown1(page) ,context={'body': markdown1(page)} )
+#@app.route('/<newpage>')
+#
+#def markdown1(page):
+#    template_folder = path.abspath('./wiki/markdown')
+#    md = markdown.Markdown(
+#    extensions=[
+#            'markdown.extensions.extra',
+#            'markdown.extensions.toc'
+#        ]
+#    )
+#    html = md.convert(text)
+#    page = render_template(context={'body' : html, 'toc': md.toc})
+#    return render_template(str(Path('markdown') / (page.lower() + '.html')))
+#
+#def pages(page):
+#    return render_template(markdown1(page) ,context={'body': markdown1(page)} )
+####
 
 # Main Function, Runs at http://0.0.0.0:8080
 if __name__ == "__main__":
