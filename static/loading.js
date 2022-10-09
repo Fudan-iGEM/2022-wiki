88a49b4f4c4573600e72df7ccdf453ed175424e1
Zihan Zhang <2364065915@qq.com>
2022-10-09 18:43:12 +0800

    loading.js err

--- static/loading.js
+++ static/loading.js
@@ -1,12 +1,15 @@
-window.addEventListener("load", () => {
-    setTimeout(function (){
-        const loader = document.querySelector(".loader");
-        if (loader !== null) {
-          loader.classList.add("loader--hidden");
-          loader.addEventListener("transitionend", () => {
-            document.body.removeChild(loader);
-          });
+window.addEventListener('load', () => {
+  setTimeout(function () {
+    const loader = document.querySelector('.loader');
+    if (loader !== null) {
+      loader.classList.add('loader--hidden');
+      loader.addEventListener('transitionend', () => {
+        try {
+          document.body.removeChild(loader);
+        } catch (err) {
+          console.log('loading.js', err);
         }
-    },1000)
-
+      });
+    }
+  }, 1000);
 });
