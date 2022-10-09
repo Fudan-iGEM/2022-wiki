b24054d9a16cf0b62ba24505f9008c679160ef94
Zihan Zhang <2364065915@qq.com>
2022-10-09 18:50:53 +0800

    div a, in ladingpage

--- static/landingpage.js
+++ static/landingpage.js
@@ -1,20 +1,20 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  // footer, div > a
+  $(document).delegate('div', 'click', function () {
+    window.location = $(this).find('a').attr('href');
+  });
 
-    $(window).on('scroll',function(){
-
-        var link = $('#navbar a.dot');
-        var top = $(window).scrollTop();
-
-        $('.sec').each(function(){
-            var id = $(this).attr('id');
-            var height = $(this).height();
-            var offset = $(this).offset().top - 150;
-            if(top >= offset && top < offset + height){
-                link.removeClass('active');
-                $('#navbar').find('[data-scroll="' + id + '"]').addClass('active');
-            }
-        });
-
+  $(window).on('scroll', function () {
+    const link = $('#navbar a.dot');
+    const top = $(window).scrollTop();
+    $('.sec').each(function () {
+      const id = $(this).attr('id');
+      const height = $(this).height();
+      const offset = $(this).offset().top - 150;
+      if (top >= offset && top < offset + height) {
+        link.removeClass('active');
+        $('#navbar').find('[data-scroll="' + id + '"]').addClass('active');
+      }
     });
-
+  });
 });
