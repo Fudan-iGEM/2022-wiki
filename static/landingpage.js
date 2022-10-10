ab43cd0e4ab064d31bb180066fc500a5fbc48d86
Zihan Zhang <2364065915@qq.com>
2022-10-10 10:30:46 +0000

    Update static/landingpage.js, wiki/pages/software.html

--- static/landingpage.js
+++ static/landingpage.js
@@ -17,29 +17,18 @@ $(document).ready(function () {
       }
     });
   });
+  $(window).on('scroll', function(){
+    const link = $('#landingpage a');
+    const top = $(window).scrollTop();
+    $('.sec').each(function () {
+      const id = $(this).attr('id');
+      const height = $(this).height();
+      const offset = $(this).offset().top - 150;
+      if (top >= offset && top < offset + height) {
+        link.removeClass('active');
+        $('#landingpage').find('[data-scroll="' + id + '"]').addClass('active');
+      }
+    });
+  });
 });
 
-const anchors = document.getElementsByClassName('anchor')
-    const contents = document.getElementsByClassName('content')
-
-    const ob = new IntersectionObserver(changes => {
-      const change = changes[0]
-      if (change.isIntersecting) {
-        const index = change.target.id.slice(-1)
-        window.history.pushState({ key: 'content' }, '', `#${change.target.id}`)
-        for (let i = 0; i < anchors.length; i++) {
-          console.log(index)
-          if (i == index) {
-            anchors.item(i).classList.add('active')
-          } else {
-            anchors.item(i).classList.remove('active')
-          }
-        }
-      }
-    }, {
-      threshold: 0.6
-    })
-
-    for (const item of contents) {
-      ob.observe(item)
-    }
