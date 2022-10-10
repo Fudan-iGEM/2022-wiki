0ca196f8834cfb6c61270bfe493a1dff78da3c9d
Zihan Zhang <2364065915@qq.com>
2022-10-10 23:46:18 +0000

    Update static/landingpage.js

--- static/landingpage.js
+++ static/landingpage.js
@@ -31,6 +31,7 @@ $(document).ready(function () {
     });
   });
 let arrow = document.querySelectorAll(".arrow");
+console.log(arrow)
   for (var i = 0; i < arrow.length; i++) {
     arrow[i].addEventListener("click", (e)=>{
       console.log(arrow)
