b5e0f42b788cc85c4ce70e53f919ee8626e6caea
Zihan Zhang <2364065915@qq.com>
2022-10-11 19:51:55 +0000

    Update wiki/pages/index.html, static/wheelmenu.js, static/wheel-menu.css, static/divflow.css

--- static/wheelmenu.js
+++ static/wheelmenu.js
@@ -1,82 +1,87 @@
-const toggle = document.querySelector('.toggle');
-const menu = document.querySelector('.menu');
-const toggle1 = document.querySelector('.toggle1');
-const menuitem = document.querySelector('.menuitem');
-let oldpositionx;
-let oldpositiony;
-let offsetX = 0;
-let offsetY = 0;
+let toggle = document.querySelector('.toggle')
+let menu = document.querySelector('.menu')
+let menuitem = document.querySelector('.menuitem')
+let menuitem_second = document.querySelector('.menuitem_second')
+let oldpositionx ;
+let oldpositiony ;
+let offsetX = 0, offsetY = 0;
 
 const move = (e) => {
-    if (!menu.classList.contains("active") && !menuitem.classList.contains("active")) {
+    if (!menu.classList.contains("active")&&!menuitem.classList.contains("active")) {
         if (e.type === "touchmove") {
-            ////console.log(e)
+            console.log(e)
             menu.style.top = e.touches[0].clientY - offsetY + "px";
             menu.style.left = e.touches[0].clientX - offsetX + "px";
-            e.preventDefault();
+            e.preventDefault()
         } else {
             menu.style.top = e.clientY - offsetY + "px";
             menu.style.left = e.clientX - offsetX + "px";
-            e.preventDefault();
+            e.preventDefault()
         }
     }
 };
 
 const mouseDown = (e) => {
-    ////console.log("mouse down ");
+    console.log("mouse down ");
+
     if (e.type === "mousedown") {
         offsetY = e.clientY - menu.getBoundingClientRect().top;
         offsetX = e.clientX - menu.getBoundingClientRect().left;
         window.addEventListener("mousemove", move);
         oldpositionx = menu.style.left;
         oldpositiony = menu.style.top;
-        e.preventDefault();
+        e.preventDefault()
     } else {
         offsetY = e.touches[0].clientY - menu.getBoundingClientRect().top;
         offsetX = e.touches[0].clientX - menu.getBoundingClientRect().left;
         window.addEventListener("touchmove", move);
         oldpositionx = menu.style.left;
         oldpositiony = menu.style.top;
-        e.preventDefault();
+        e.preventDefault()
     }
     menu.style.transition = "none";
 };
 
 const mouseUp = (e) => {
-    ////console.log("mouse up");
+    console.log("mouse up");
     if (e.type === "mouseup") {
         window.removeEventListener("mousemove", move);
-        e.preventDefault();
+        offsetX = 0, offsetY = 0;
+        e.preventDefault()
     } else {
         window.removeEventListener("touchmove", move);
-        e.preventDefault();
+        offsetX = 0, offsetY = 0;
+        e.preventDefault()
+
     }
-    offsetX = 0;
-    offsetY = 0;
     snapToSide(e)
     menu.style.transition = "0.3s ease-in-out left";
 };
 
 const snapToSide = (e) => {
     const windowWidth = window.innerWidth;
-    let currPositionX; //, currPositionY;
+    let currPositionX, currPositionY;
     if (e.type === "touchend") {
         currPositionX = e.changedTouches[0].clientX;
-        ////currPositionY = e.changedTouches[0].clientY;
+        currPositionY = e.changedTouches[0].clientY;
     } else {
         currPositionX = e.clientX;
-        ////currPositionY = e.clientY;
+        currPositionY = e.clientY;
     }
     if (currPositionX < windowWidth / 2) {
         menu.style.left = 0 + "px";
         menuitem.classList.remove('right');
         menuitem.classList.add('left');
+        menuitem_second.classList.remove('right');
+        menuitem_second.classList.add('left');
         menu.classList.remove('right');
         menu.classList.add('left');
     } else {
         menu.style.left = windowWidth - 200 + "px";
         menuitem.classList.remove('left');
         menuitem.classList.add('right');
+        menuitem_second.classList.remove('left');
+        menuitem_second.classList.add('right');
         menu.classList.remove('left');
         menu.classList.add('right');
     }
@@ -90,35 +95,38 @@ menu.addEventListener("touchstart", mouseDown);
 
 menu.addEventListener("touchend", mouseUp);
 
-toggle.addEventListener("click", (e) => {
-    if (oldpositionx === menu.style.left &&
-        oldpositiony === menu.style.top) {
+toggle.addEventListener("click", (e) =>{
+    if (oldpositionx === menu.style.left&&
+    oldpositiony === menu.style.top){
         if (menuitem.classList.contains('active')) {
+
+
             menuitem.classList.toggle('active');
-            menu.classList.toggle('active');
+            menu.classList.toggle('active')
+            menuitem_second.classList.toggle('active');
+
         } else {
             menu.classList.toggle('active');
         }
     }
-});
+})
 
-toggle.addEventListener("touchend", (e) => {
-    if (oldpositionx === menu.style.left &&
-        oldpositiony === menu.style.top) {
+toggle.addEventListener("touchend", (e) =>{
+    if (oldpositionx === menu.style.left&&
+        oldpositiony === menu.style.top){
         if (menuitem.classList.contains('active')) {
             menuitem.classList.toggle('active');
-            menu.classList.toggle('active');
+            menuitem_second.classList.toggle('active');
+            menu.classList.toggle('active')
+
         } else {
             menu.classList.toggle('active');
         }
     }
-});
+})
 
-toggle1.addEventListener("click", (e) => {
-    menu.classList.toggle('active');
-    menuitem.classList.toggle('active');
-});
-toggle1.addEventListener("touchend", (e) => {
-    menu.classList.toggle('active');
-    menuitem.classList.toggle('active');
-});
+
+
+
+
+
