4216cef7098f37f5617b5905230de6615651d653
Zihan Zhang <2364065915@qq.com>
2022-10-12 03:28:24 +0000

    Update static/divflow.js, wiki/pages/index.html, static/divflow.css

--- static/divflow.js
+++ static/divflow.js
@@ -3,60 +3,84 @@ const tl2 = new TimelineMax({onUpdate:updatePercentage});
 const tl3 = new TimelineMax({onUpdate:updatePercentage});
 const tl4 = new TimelineMax({onUpdate:updatePercentage});
 const tl5 = new TimelineMax({onUpdate:updatePercentage});
+const tl6 = new TimelineMax({onUpdate:updatePercentage});
+const tl7 = new TimelineMax({onUpdate:updatePercentage});
 
 const controller1 = new ScrollMagic.Controller();
 const controller2 = new ScrollMagic.Controller();
 const controller3 = new ScrollMagic.Controller();
 const controller4 = new ScrollMagic.Controller();
 const controller5 = new ScrollMagic.Controller();
+const controller6 = new ScrollMagic.Controller();
+const controller7 = new ScrollMagic.Controller();
 
-tl1.from('blockquote', 0.5, { x: 200, opacity: 0 });
-tl1.from('span', 1, { width: 0 }, '=-.5');
-tl1.from('#yellow', 1, { x: 300, opacity: 0 }, '=-1');
-tl1.from('#orange1', 1, { x: -200, opacity: 0 });
+tl1.from('#mask1', 0.5, { y: -200, opacity: 0 });
+tl1.from('#mask2', 1, { y: -300, opacity: 0 }, '=-.5');
 
-tl2.from('#lightgreen', 1, { x: -200, opacity: 0 });
-tl3.from('#darkgreen1', 1, { x: -200, opacity: 0 });
+tl2.from('#route_word1', 1, { y: -600, opacity: 0 });
+tl2.from('#route_word2', 1, { x: -400, opacity: 0 });
+tl2.from('#route_word3', 1, { x: -200, opacity: 0 });
 
-tl4.from('#orange2', 1, { x: -200, opacity: 0 });
-tl5.from('#orange3', 1, { x: -200, opacity: 0 });
-tl5.from('#darkgreen2', 1, { x: -200, opacity: 0 });
+tl3.from('#reasons1', 1, { x: -300, opacity: 0})
+tl3.from('#reasons2', 1, { x: -300, opacity: 0})
+tl3.from('#reasons3', 1, { x: 300, opacity: 0})
+tl3.from('#reasons4', 1, { x: 300, opacity: 0})
+
+tl4.from('#women_men1', 1, { x: -300, opacity: 0})
+tl4.from('#women_men2', 1, { x: -300, opacity: 0})
+tl4.from('#women_men3', 1, { x: 300, opacity: 0})
+tl4.from('#women_men4', 1, { x: 300, opacity: 0})
+
+tl5.from('#ideas1', 1, { x: -300, opacity: 0})
+tl5.from('#ideas2', 1, { x: -300, opacity: 0})
+tl5.from('#ideas3', 1, { x: 300, opacity: 0})
+tl5.from('#ideas4', 1, { x: 300, opacity: 0})
+
 
 const scene1 = new ScrollMagic.Scene({
-    triggerElement: "#animation",
+    triggerElement: "#mask",
     triggerHook: "onLeave",
     duration: "100%"
 })
-    .setPin("#animation")
+    .setPin("#mask")
     .setTween(tl1)
         .addTo(controller1);
 
 const scene2 = new ScrollMagic.Scene({
-    triggerElement: "#animation1",
+    triggerElement: "#route_word",
     triggerHook: "onLeave",
     duration: "100%"
 })
-    .setPin("#animation1")
+    .setPin("#route_word")
     .setTween(tl2)
     .addTo(controller2);
 
 const scene3 = new ScrollMagic.Scene({
-    triggerElement: "#animation2",
+    triggerElement: "#reasons",
     triggerHook: "onLeave",
     duration: "100%"
 })
-    .setPin("#animation2")
+    .setPin("#reasons")
     .setTween(tl3)
     .addTo(controller3);
 
 const scene4 = new ScrollMagic.Scene({
-    triggerElement: "#animation3",
+    triggerElement: "#women_men",
     triggerHook: "onLeave",
     duration: "100%"
 })
-    .setPin("#animation3")
+    .setPin("#women_men")
     .setTween(tl4)
     .addTo(controller4);
+
+const scene5 = new ScrollMagic.Scene({
+    triggerElement: "#ideas",
+    triggerHook: "onLeave",
+    duration: "100%"
+})
+    .setPin("#ideas")
+    .setTween(tl5)
+    .addTo(controller5);
 
 const scene5 = new ScrollMagic.Scene({
     triggerElement: "#animation4",
@@ -67,12 +91,24 @@ const scene5 = new ScrollMagic.Scene({
     .setTween(tl5)
     .addTo(controller5);
 
-function updatePercentage () {
-    //percent.innerHTML = (tl.progress() *100 ).toFixed();
-    tl1.progress();
-    tl2.progress();
-    tl3.progress();
-    tl4.progress();
-    tl5.progress();
-    ////console.log(tl2.progress());
-}
+$(function () { // wait for document ready
+		// init
+		var controller = new ScrollMagic.Controller();
+
+		// define movement of panels
+		var wipeAnimation = new TimelineMax()
+			// animate to second panel
+			.to("#slidecontainer", 0.5, {z: -150})		// move back in 3D space
+			.to("#slidecontainer", 1,   {x: "-50%"})	// move in to first panel
+			.to("#slidecontainer", 0.5, {z: 0})				// move back to origin in 3D space;
+
+		// create scene to pin and link animation
+		new ScrollMagic.Scene({
+				triggerElement: "#pincontainer",
+				triggerHook: "onLeave",
+				duration: "500%"
+			})
+			.setPin("#pincontainer")
+			.setTween(wipeAnimation)
+			.addTo(controller);
+	});
