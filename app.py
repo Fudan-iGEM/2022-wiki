4220cbbf6d9b74dcef292493e81abd76b42dca42
Zihan Zhang <2364065915@qq.com>
2022-10-13 13:22:59 +0800

    compre with frozen

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from os import path
-####from pathlib import Path
+from pathlib import Path
 
 from flask import Flask, render_template
 from flask_frozen import Freezer
-####import markdown
 
 import subprocess
 __build__ = subprocess.check_output(
@@ -13,10 +12,11 @@ now = datetime.now()
 datetime_tag = now.strftime("%Y-%m-%dT%H:%M:%SZ")
 datetime_str = now.strftime("%d/%m/%Y, %H:%M:%S")
 
+
 template_folder = path.abspath('./wiki')
 
 app = Flask(__name__, template_folder=template_folder)
-#app.config['FREEZER_BASE_URL'] = environ.get('CI_PAGES_URL') ## build fail w/ unknown environ
+#app.config['FREEZER_BASE_URL'] = environ.get('CI_PAGES_URL')
 app.config['FREEZER_DESTINATION'] = 'public'
 app.config['FREEZER_RELATIVE_URLS'] = True
 app.config['FREEZER_IGNORE_MIMETYPE_WARNINGS'] = True
@@ -37,26 +37,8 @@ def index():
 
 @app.route('/<page>')
 def pages(page):
-    return render_template('pages' + str(page.lower()) + '.html',
-                           __build__=__build__, datetime_str=datetime_str, datetime_tag=datetime_tag)
-
-#@app.route('/<newpage>')
-#
-#def markdown1(page):
-#    template_folder = path.abspath('./wiki/markdown')
-#    md = markdown.Markdown(
-#    extensions=[
-#            'markdown.extensions.extra',
-#            'markdown.extensions.toc'
-#        ]
-#    )
-#    html = md.convert(text)
-#    page = render_template(context={'body' : html, 'toc': md.toc})
-#    return render_template(str(Path('markdown') / (page.lower() + '.html')))
-#
-#def pages(page):
-#    return render_template(markdown1(page) ,context={'body': markdown1(page)} )
-####
+    return render_template(str(Path('pages')) + '/' + page.lower() + '.html',
+                           __build__=__build__, datetime_str=datetime_str, datetime_tag=datetime_tag )
 
 # Main Function, Runs at http://0.0.0.0:8080
 if __name__ == "__main__":
